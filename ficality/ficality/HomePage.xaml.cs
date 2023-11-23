59846b1e6af6fffcf49bbc9631f5ee6c3d921bb3
hasan-elbadry <hasan.elbadry2006@gmail.com>
2023-11-23 19:33:01 +0200

    all project

---
+++ ficality/ficality/HomePage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ficality
+{
+    /// <summary>
+    /// Interaction logic for HomePage.xaml
+    /// </summary>
+    public partial class HomePage : Page
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+        UniveristyEntities1 db=new UniveristyEntities1();
+        private void opendata_btn_Click(object sender, RoutedEventArgs e)
+        {
+            SearchPage searchPage = new SearchPage();
+            this.NavigationService.Navigate(searchPage);
+        }
+
+        private void savedata_btn_Click(object sender, RoutedEventArgs e)
+        {
+            student_table student= new student_table();
+            if (name_txt.Text != "")
+            {
+                student.stu_name = name_txt.Text;
+                student.stu_age = age_txt.Text;
+                student.stu_address = adress_txt.Text;
+                student.stu_department = department_txt.Text;
+                db.student_table.Add(student);
+                db.SaveChanges();
+                MessageBox.Show("successful add");
+            }
+            else
+            {
+                MessageBox.Show("please enter data");
+            }
+        }
+    }
+}
---
+++ ficality/ficality/SearchPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ficality
+{
+    /// <summary>
+    /// Interaction logic for SearchPage.xaml
+    /// </summary>
+    public partial class SearchPage : Page
+    {
+        UniveristyEntities1 db=new UniveristyEntities1();
+        public SearchPage()
+        {
+            InitializeComponent();
+            combo.ItemsSource = db.student_table.Select(x=>x.stu_department).Distinct().ToList();
+            
+        }
+
+        private void combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            datagridd.ItemsSource =7null;
+        }
+
+        private void search_btn_Click(object sender, object e)
+        {
+            var dep = combo.SelectedItem;
+            if (dep==null) {
+                dep = "";
+
+            }      
+                datagridd.ItemsSource = db.student_table.Where(x => x.stu_name.Contains(search_txt.Text)).ToList();
+        }
+    }
+}
